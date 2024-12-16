42b186d4dc37faae23cf93ac1eb4a558b03acb79
PavithraGitCode <pavithrapavimbca@gmail.com>
2024-12-16 10:39:13 +0530

    new changes added

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const elements = document.querySelectorAll('.cont-border, .box');
-
 elements.forEach(contBorder => {
   const firstCont = contBorder.querySelector('.first-cont');   
   const offerContent = contBorder.querySelector('.offer-content');  
@@ -7,31 +6,19 @@ elements.forEach(contBorder => {
   const allOfferContents = document.querySelectorAll('.offer-content');
   const allContBorders = document.querySelectorAll('.cont-border, .box');
   const allRadioButtons = document.querySelectorAll('input[type="radio"]');
-
   firstCont.addEventListener('click', function() {
-  
     allOfferContents.forEach(content => {
       content.style.display = 'none';
     });
-    
- 
     allContBorders.forEach(border => {
       border.style.borderColor = '#C8C8C8';
       border.style.backgroundColor = '';  
     });
-
-     
     allRadioButtons.forEach(radio => {
       radio.checked = false;
     });
-
-   
     radioButton.checked = true;
-
-     
     offerContent.style.display = (offerContent.style.display === 'block') ? 'none' : 'block';
-
-     
     contBorder.style.borderColor = (contBorder.style.borderColor === 'rgb(255, 107, 130)') ? '#C8C8C8' : '#FF6B82';
     contBorder.style.backgroundColor = (contBorder.style.backgroundColor === 'rgb(255, 249, 250)') ? '' : '#FFF9FA';  
   });
